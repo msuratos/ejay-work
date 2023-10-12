80bb660664a882037eb31997f6e696edd49e7804
Melvin Suratos <melvinsuratos@hotmail.com>
2023-10-11 21:20:34 -0700

    changed the logic to activate based on switch toggle also, changed the logic so that every toggle will only run a certain sequence once until a change in the switch state has occurred.

--- ejay-work-project.ino
+++ ejay-work-project.ino
@@ -6,27 +6,28 @@
 Stepper stepper(STEPS, 2, 3); // Pin 2 connected to DIRECTION & Pin 3 connected to STEP Pin of Driver
 
 bool clockwise = true;
+int switchPin = 7;
+int currentSwitchState = LOW;
 
 void setup() {
   // Set the maximum speed in steps per second:
   stepper.setSpeed(500);
-  pinMode(LED_BUILTIN, OUTPUT);
 
-  // LED is on by default
-  digitalWrite(LED_BUILTIN, HIGH);
+  // Setting pin mode for LED & switch pin
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(switchPin, INPUT);
+
+  // LED is off by default
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
-  if (clockwise) {
-    stepper.step(800*15);   // 800 is 360 deg rotation. 800 * 15 means 15 rotations
+  if (digitalRead(switchPin) != currentSwitchState) {
+    currentSwitchState = digitalRead(switchPin);
+    digitalWrite(LED_BUILTIN, currentSwitchState);  // LED is on or off depending on current switch state
+
+    stepper.step(800*8);    // 800 is 360 deg rotation. 800 * 15 means 15 rotations
     delay(5000);
-    digitalWrite(LED_BUILTIN, LOW);
-    clockwise = false;
-  }
-  else {
-    stepper.step(-800*15);     // 800 is 360 deg rotation. 800 * 15 means 15 rotations
-    delay(5000);
-    digitalWrite(LED_BUILTIN, HIGH);
-    clockwise = true;
+    stepper.step(-800*8);   // 800 is 360 deg rotation. 800 * 15 means 15 rotations
   }
 }
