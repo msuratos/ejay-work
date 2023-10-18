f9d14deb6d333bdf3a5aabb39df08375dcbb8e76
Melvin Suratos <melvinsuratos@hotmail.com>
2023-10-17 22:41:17 -0700

    adding limit switch logic

--- ejay-work.ino
+++ ejay-work.ino
@@ -1,8 +1,16 @@
-#include <Stepper.h> 
+#include <Stepper.h>
+#include <ezButton.h>
+
 #define STEPS 200
 
 // Define stepper motor connections and motor interface type. Motor interface type must be set to 1 when using a driver
 Stepper stepper(STEPS, 2, 3); // Pin 2 connected to DIRECTION & Pin 3 connected to STEP Pin of Driver
+
+// Create ezButton object that attach to pin 7 for the top switch
+ezButton topLimitSwitch(7);
+
+// Create ezButton object that attach to pin 8 for the bottom switch
+ezButton bottomLimitSwitch(8);
 
 // boolean variable used to determine which direction the motor should spin
 bool clockwise = true;
@@ -10,6 +18,12 @@ bool clockwise = true;
 void setup() {
   // Set the maximum speed in steps per second:
   stepper.setSpeed(500);
+
+  // Set debounce time to 50 milliseconds
+  topLimitSwitch.setDebounceTime(50);
+  bottomLimitSwitch.setDebounceTime(50);
+
+  // Set built-in LED pin to OUTPUT
   pinMode(LED_BUILTIN, OUTPUT);
 
   // LED is off by default. If LED is off, it means the motor is waiting to be moved,
@@ -18,6 +32,26 @@ void setup() {
 }
 
 void loop() {
+  // MUST call the loop() function first
+  topLimitSwitch.loop();
+  bottomLimitSwitch.loop();
+
+  int topSwitchState = topLimiteSwitch.getState();
+  int bottomSwitchState = bottomLimitSwitch.getState();
+
+  // top switch is pressed and bottom switch is not pressed
+  // if (topSwitchState == LOW) {
+  //   clockwise = true;
+  // }
+
+  // if (topSwitchState.isPressed()) {
+  //   clockwise = true;
+  // }
+
+  // if (topSwitchState.isRelease()) {
+  //   clockwise = false;
+  // }
+
   if (clockwise) {
     digitalWrite(LED_BUILTIN, HIGH);  // turn LED on, meaning the motor is moving
     stepper.step(800*8);              // 800 is 360 deg rotation. 800 * 8 means 8 rotations
